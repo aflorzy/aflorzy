b2df194e8af22a9da25c8c69c119608818f546b6
Andrew <andrewtflores@yahoo.com>
2024-06-15 12:04:18 -0500

    Add layout components and styles, and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { HomeComponent } from './components/home/home.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomeComponent
+    },
+    {
+        path: "blog",
+        component: BlogComponent
+    },
+    {
+        path: "portfolio",
+        component: PortfolioComponent
+    }
+];
